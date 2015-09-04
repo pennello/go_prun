a6d4d06ef6b72ccd4f3c52dcc66308b3896b834a
Chris Pennello <git@chrispennello.com>
2015-09-03 19:43:41 -0700

    cmd: Adds documentation for arg functions.

--- cmd/arg.go
+++ cmd/arg.go
@@ -7,11 +7,14 @@ import (
 	"os"
 )
 
+// BadArgs logs with the given format and additional arguments and exits
+// the process with exit status 2.
 func BadArgs(format string, a ...interface{}) {
 	log.Printf(format, a...)
 	os.Exit(2)
 }
 
+// ArgError calls BadArgs with the given error.
 func ArgError(err error) {
 	BadArgs("%v\n", err)
 }
